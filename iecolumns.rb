da9c3b1d7bf6da6a0fb0f20c6af664adbc420817
spacefra <spacefra@shirlock.site5.com>
2009-08-12 13:46:20 -0700

    Made iexpress include cp_convert and cleaned it up

--- iecolumns.rb
+++ iecolumns.rb
@@ -4,62 +4,9 @@ require 'hpricot'
 require 'md5'
 require 'erb'
 require 'cgi'
+require 'cp_convert'
 
-$KCODE = 'u'
-require 'jcode'
-
-class UString < String
-   # Show u-prefix as in Python
-   def inspect; "u#{ super }" end
-
-   # Count multibyte characters
-   def length; self.scan(/./).length end
-
-   # Reverse the string
-   def reverse; self.scan(/./).reverse.join end
- end
-
- module Kernel
-   def u( str )
-     UString.new str.gsub(/U\+([0-9a-fA-F]{4,4})/u){["#$1".hex ].pack('U*')}
-   end
- end 
-
-
-CP_MAP = {
-    # from http=>//www.microsoft.com/typography/unicode/1252.htm
-    "\x80"=> "U+20AC", # EURO SIGN
-    "\x82"=> "U+201A", # SINGLE LOW-9 QUOTATION MARK
-    "\x83"=> "U+0192", # LATIN SMALL LETTER F WITH HOOK
-    "\x84"=> "U+201E", # DOUBLE LOW-9 QUOTATION MARK
-    "\x85"=> "U+2026", # HORIZONTAL ELLIPSIS
-    "\x86"=> "U+2020", # DAGGER
-    "\x87"=> "U+2021", # DOUBLE DAGGER
-    "\x88"=> "U+02C6", # MODIFIER LETTER CIRCUMFLEX ACCENT
-    "\x89"=> "U+2030", # PER MILLE SIGN
-    "\x8A"=> "U+0160", # LATIN CAPITAL LETTER S WITH CARON
-    "\x8B"=> "U+2039", # SINGLE LEFT-POINTING ANGLE QUOTATION MARK
-    "\x8C"=> "U+0152", # LATIN CAPITAL LIGATURE OE
-    "\x8E"=> "U+017D", # LATIN CAPITAL LETTER Z WITH CARON
-    "\x91"=> "U+2018", # LEFT SINGLE QUOTATION MARK
-    "\x92"=> "U+2019", # RIGHT SINGLE QUOTATION MARK
-    "\x93"=> "U+201C", # LEFT DOUBLE QUOTATION MARK
-    "\x94"=> "U+201D", # RIGHT DOUBLE QUOTATION MARK
-    "\x95"=> "U+2022", # BULLET
-    "\x96"=> "U+2013", # EN DASH
-    "\x97"=> "U+2014", # EM DASH
-    "\x98"=> "U+02DC", # SMALL TILDE
-    "\x99"=> "U+2122", # TRADE MARK SIGN
-    "\x9A"=> "U+0161", # LATIN SMALL LETTER S WITH CARON
-    "\x9B"=> "U+203A", # SINGLE RIGHT-POINTING ANGLE QUOTATION MARK
-    "\x9C"=> "U+0153", # LATIN SMALL LIGATURE OE
-    "\x9E"=> "U+017E", # LATIN SMALL LETTER Z WITH CARON
-    "\x9F"=> "U+0178", # LATIN CAPITAL LETTER Y WITH DIAERESIS
-}
-
-CP1252 = CP_MAP.keys.join
-UTF = CP_MAP.values.join
-
+include CP1252
 
 def h(txt)
   CGI.escapeHTML txt
@@ -82,14 +29,9 @@ def parse_articles(doc)
       href = href+"0"
     end
     hlt=c.at("a").html
-    hlt=hlt.tr(CP1252,u(UTF))
+    hlt=kill_gremlins(hlt)
     txt=b.children.last.to_s
-    #txt = Iconv.iconv("UTF-8", "CP1252", txt+' ')
-    #txt = Iconv.iconv("UTF-8//IGNORE", "WINDOWS-1252", txt+' ')
-    #txt = Iconv.iconv("UTF-8//IGNORE", "UTF-8", txt+' ')
-    #ic=Iconv.new('UTF-8//IGNORE', 'WINDOWS-1252')
-    #txt = ic.iconv(txt+' ')[0..-2]
-    txt=txt.tr(CP1252,u(UTF))
+    txt=kill_gremlins(txt)
     
     parts=b.at("p").children.last.to_s.split
     parts.last.sub!(/\'/,'20')
