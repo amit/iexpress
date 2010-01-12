ce046f40f79119e1a2189abc54db40ba1980aa0d
spacefra <spacefra@shirlock.site5.com>
2010-01-11 17:46:23 -0800

    Fix a bug in dnaindia (deal with absolute url)

--- dnacolumn.rb
+++ dnacolumn.rb
@@ -3,6 +3,15 @@ require 'rubygems'
 require 'hpricot'
 require 'md5'
 require 'erb'
+require 'cgi'
+require 'cp_convert'
+
+include CP1252
+
+def h(txt)
+  CGI.escapeHTML txt
+end
+
 
 $debug = 0
 
@@ -12,34 +21,13 @@ def parse_dna_articles(doc)
   items=doc/"//div[@class1=content]"
   items.each{ |b|
     txt=b.at("div.syn2").html
+    txt=kill_gremlins(txt)
     hlt=b.at("h4/a").html
+    hlt=kill_gremlins(hlt)
     href=b.at("h4/a")["href"]
-    href="http://www.dnaindia.com"+href
+    #href="http://www.dnaindia.com"+href
     dt=Date.parse((b/"div").last.html)
-    updated=dt.strftime("%Y-%m-%dT06:00:#{sprintf("%02d",snap)}Z")
-    snap=snap-1
-    articles << {:title=>hlt, :body=>txt, :updated=>updated, :permalink=>href}
-  }
-  return articles
-end
-
-def parse_articles(doc)
-  box=doc.at "#box_left"
-  all=box/"div.listing_content"
-  # Dumb hack
-  snap=59
-  articles = []
-  all.each { |b|
-    c=b.at("h5")
-    href=c.at("a")['href']
-    if /.*news.*\/\d+\//.match href
-      href = href+"0"
-    end
-    hlt=c.at("a").html
-    txt=b.children.last.to_s
-    parts=b.at("p").children.last.to_s.split
-    parts.last.sub!(/\'/,'20')
-    updated=Date.parse(parts.join(' ')).strftime("%Y-%m-%dT06:00:#{sprintf("%02d",snap)}Z")
+    updated=dt.strftime("%Y-%m-%dT00:01:#{sprintf("%02d",snap)}+05:30")
     snap=snap-1
     articles << {:title=>hlt, :body=>txt, :updated=>updated, :permalink=>href}
   }
@@ -47,7 +35,6 @@ def parse_articles(doc)
 end
 
 def main
-
 if ARGV.length != 1
   puts "Usage: #{$0} columnist"
   exit(1)
@@ -67,7 +54,7 @@ if File.file? digestfile
 else
   cached_digest = 0
 end
-data = URI(base).read("User-Agent" => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9',"Referer" => 'http://www.iexpress.com/')
+data = URI(base).read("User-Agent" => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.9) Gecko/20071025 Firefox/2.0.0.9',"Referer" => 'http://www.dnsindia.com/')
 #data = File.read('index.html')
 dgst = MD5.new(data).hexdigest
 STDERR.puts "Digest = #{dgst}" if $debug == 1
@@ -84,10 +71,6 @@ doc = Hpricot(data)
 
 rssdata=  parse_dna_articles(doc)
 
-#rssdata.each{|f|
-#puts f[:title]
-#}
-
 o=File.open("/home/spacefra/www/feeds/#{name}.atom",'w')
 generate_atom rssdata,o,name
 
@@ -99,7 +82,7 @@ feed_id = "http://www.dnaindia.com/columns/#{name}/"
 feed_title = "Columnists: #{name}"
 feed_updated = rssdata[0][:updated]
 feed_link = "http://chakradeo.net/feeds/#{name}.atom"
-feed_author = "Amit"
+feed_author = "Amit Chakradeo"
 feed_entries = rssdata
 
 hdl.puts(temp.result(binding()))
